c61ddbf643e670a0645e2ccf7dc4473360494b80
bobeyuno <bobeyuno@gmail.com>
2014-07-28 23:09:18 +0100

    add proxy package

---
+++ src/main/java/com/bobeyuno/letsmodreboot/proxy/ClientProxy.java
@@ -0,0 +1,7 @@
+package com.bobeyuno.letsmodreboot.proxy;
+
+/**
+ * Created by Jack on 28/07/2014.
+ */
+public class ClientProxy {
+}
---
+++ src/main/java/com/bobeyuno/letsmodreboot/proxy/CommonProxy.java
@@ -0,0 +1,9 @@
+package com.bobeyuno.letsmodreboot.proxy;
+
+/**
+ * Created by Jack on 28/07/2014.
+ */
+public class CommonProxy implements IProxy
+{
+
+}
